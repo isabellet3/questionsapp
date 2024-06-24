07ed5137569a9605767f9dbc5913baeec7a34ff8
Isabelle Tuyay <iylabelle03@gmail.com>
2024-06-24 10:10:08 -0500

    Initial Commit

---
+++ questionsapp/questionsappApp.swift
@@ -0,0 +1,17 @@
+//
+//  questionsappApp.swift
+//  questionsapp
+//
+//  Created by Scholar on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct questionsappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
